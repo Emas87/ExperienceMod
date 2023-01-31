ac3e9a1d54f76de06ec6504dc833f62524b7cd4d
Ema <emanu.87@gmail.com>
2023-01-30 18:31:23 -0600

    Adding mod core

---
+++ Items/ExpGlobalItem.cs
@@ -0,0 +1,366 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using System;
+using System.Collections.Generic;
+using Terraria.Utilities;
+using rail;
+
+namespace Experience.Items
+{
+    internal class ExpGlobalItem : GlobalItem
+    {
+        public int[] experience;
+        public override bool InstancePerEntity => true;
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (experience != null)
+            {//experience may be null if spawned from other mods which don't call OnCreate
+
+                TooltipLine tooltipLine = new TooltipLine(Mod, "Experience","Level: " + experience[1] + "\nExperience : " + experience[0].ToString() + "\nExperience for next level: " + experience[3].ToString()) { OverrideColor = Color.LightGreen };
+                tooltips.Add(tooltipLine);
+
+                // Fix damage percentage by Prefix
+                foreach (var tooltip in tooltips)
+                {            
+                    if (tooltip.Name == "PrefixDamage")
+                    {
+                        TryGetPrefixStatMultipliersForItem(item, out float dmg, out float kb, out float spd, out float size, out float shtspd, out float mcst, out int crt, out string changes);
+                        if (changes.Contains("dmg"))
+                        {
+                            int damagePercentage = (int)Math.Round((dmg - 1f) * 100);
+                            tooltip.Text = "+" + damagePercentage.ToString() + "% damage";
+                        } else {
+                            tooltips.Remove(tooltip);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        public bool TryGetPrefixStatMultipliersForItem(Item item, out float dmg, out float kb, out float spd, out float size, out float shtspd, out float mcst, out int crt, out string changes)
+        {
+            int rolledPrefix = item.prefix;
+            dmg = 1f;
+            kb = 1f;
+            spd = 1f;
+            size = 1f;
+            shtspd = 1f;
+            mcst = 1f;
+            crt = 0;
+            changes = "";
+            switch (rolledPrefix)
+            {
+                case 1:
+                    size = 1.12f;
+                    break;
+                case 2:
+                    size = 1.18f;
+                    break;
+                case 3:
+                    dmg = 1.05f;
+                    crt = 2;
+                    size = 1.05f;
+                    break;
+                case 4:
+                    dmg = 1.1f;
+                    size = 1.1f;
+                    kb = 1.1f;
+                    break;
+                case 5:
+                    dmg = 1.15f;
+                    break;
+                case 6:
+                    dmg = 1.1f;
+                    break;
+                case 81:
+                    kb = 1.15f;
+                    dmg = 1.15f;
+                    crt = 5;
+                    spd = 0.9f;
+                    size = 1.1f;
+                    break;
+                case 7:
+                    size = 0.82f;
+                    break;
+                case 8:
+                    kb = 0.85f;
+                    dmg = 0.85f;
+                    size = 0.87f;
+                    break;
+                case 9:
+                    size = 0.9f;
+                    break;
+                case 10:
+                    dmg = 0.85f;
+                    break;
+                case 11:
+                    spd = 1.1f;
+                    kb = 0.9f;
+                    size = 0.9f;
+                    break;
+                case 12:
+                    kb = 1.1f;
+                    dmg = 1.05f;
+                    size = 1.1f;
+                    spd = 1.15f;
+                    break;
+                case 13:
+                    kb = 0.8f;
+                    dmg = 0.9f;
+                    size = 1.1f;
+                    break;
+                case 14:
+                    kb = 1.15f;
+                    spd = 1.1f;
+                    break;
+                case 15:
+                    kb = 0.9f;
+                    spd = 0.85f;
+                    break;
+                case 16:
+                    dmg = 1.1f;
+                    crt = 3;
+                    break;
+                case 17:
+                    spd = 0.85f;
+                    shtspd = 1.1f;
+                    break;
+                case 18:
+                    spd = 0.9f;
+                    shtspd = 1.15f;
+                    break;
+                case 19:
+                    kb = 1.15f;
+                    shtspd = 1.05f;
+                    break;
+                case 20:
+                    kb = 1.05f;
+                    shtspd = 1.05f;
+                    dmg = 1.1f;
+                    spd = 0.95f;
+                    crt = 2;
+                    break;
+                case 21:
+                    kb = 1.15f;
+                    dmg = 1.1f;
+                    break;
+                case 82:
+                    kb = 1.15f;
+                    dmg = 1.15f;
+                    crt = 5;
+                    spd = 0.9f;
+                    shtspd = 1.1f;
+                    break;
+                case 22:
+                    kb = 0.9f;
+                    shtspd = 0.9f;
+                    dmg = 0.85f;
+                    break;
+                case 23:
+                    spd = 1.15f;
+                    shtspd = 0.9f;
+                    break;
+                case 24:
+                    spd = 1.1f;
+                    kb = 0.8f;
+                    break;
+                case 25:
+                    spd = 1.1f;
+                    dmg = 1.15f;
+                    crt = 1;
+                    break;
+                case 58:
+                    spd = 0.85f;
+                    dmg = 0.85f;
+                    break;
+                case 26:
+                    mcst = 0.85f;
+                    dmg = 1.1f;
+                    break;
+                case 27:
+                    mcst = 0.85f;
+                    break;
+                case 28:
+                    mcst = 0.85f;
+                    dmg = 1.15f;
+                    kb = 1.05f;
+                    break;
+                case 83:
+                    kb = 1.15f;
+                    dmg = 1.15f;
+                    crt = 5;
+                    spd = 0.9f;
+                    mcst = 0.9f;
+                    break;
+                case 29:
+                    mcst = 1.1f;
+                    break;
+                case 30:
+                    mcst = 1.2f;
+                    dmg = 0.9f;
+                    break;
+                case 31:
+                    kb = 0.9f;
+                    dmg = 0.9f;
+                    break;
+                case 32:
+                    mcst = 1.15f;
+                    dmg = 1.1f;
+                    break;
+                case 33:
+                    mcst = 1.1f;
+                    kb = 1.1f;
+                    spd = 0.9f;
+                    break;
+                case 34:
+                    mcst = 0.9f;
+                    kb = 1.1f;
+                    spd = 1.1f;
+                    dmg = 1.1f;
+                    break;
+                case 35:
+                    mcst = 1.2f;
+                    dmg = 1.15f;
+                    kb = 1.15f;
+                    break;
+                case 52:
+                    mcst = 0.9f;
+                    dmg = 0.9f;
+                    spd = 0.9f;
+                    break;
+                case 84:
+                    kb = 1.17f;
+                    dmg = 1.17f;
+                    crt = 8;
+                    break;
+                case 36:
+                    crt = 3;
+                    break;
+                case 37:
+                    dmg = 1.1f;
+                    crt = 3;
+                    kb = 1.1f;
+                    break;
+                case 38:
+                    kb = 1.15f;
+                    break;
+                case 53:
+                    dmg = 1.1f;
+                    break;
+                case 54:
+                    kb = 1.15f;
+                    break;
+                case 55:
+                    kb = 1.15f;
+                    dmg = 1.05f;
+                    break;
+                case 59:
+                    kb = 1.15f;
+                    dmg = 1.15f;
+                    crt = 5;
+                    break;
+                case 60:
+                    dmg = 1.15f;
+                    crt = 5;
+                    break;
+                case 61:
+                    crt = 5;
+                    break;
+                case 39:
+                    dmg = 0.7f;
+                    kb = 0.8f;
+                    break;
+                case 40:
+                    dmg = 0.85f;
+                    break;
+                case 56:
+                    kb = 0.8f;
+                    break;
+                case 41:
+                    kb = 0.85f;
+                    dmg = 0.9f;
+                    break;
+                case 57:
+                    kb = 0.9f;
+                    dmg = 1.18f;
+                    break;
+                case 42:
+                    spd = 0.9f;
+                    break;
+                case 43:
+                    dmg = 1.1f;
+                    spd = 0.9f;
+                    break;
+                case 44:
+                    spd = 0.9f;
+                    crt = 3;
+                    break;
+                case 45:
+                    spd = 0.95f;
+                    break;
+                case 46:
+                    crt = 3;
+                    spd = 0.94f;
+                    dmg = 1.07f;
+                    break;
+                case 47:
+                    spd = 1.15f;
+                    break;
+                case 48:
+                    spd = 1.2f;
+                    break;
+                case 49:
+                    spd = 1.08f;
+                    break;
+                case 50:
+                    dmg = 0.8f;
+                    spd = 1.15f;
+                    break;
+                case 51:
+                    kb = 0.9f;
+                    spd = 0.9f;
+                    dmg = 1.05f;
+                    crt = 2;
+                    break;
+            }
+
+            if (dmg != 1f)
+                changes += "dmg";
+            if (kb != 1f)
+                changes += "kb,";
+            if (spd != 1f)
+                changes += "spd,";
+            if (size != 1f)
+                changes += "size,";
+            if (shtspd != 1f)
+                changes += "shtspd,";
+            if (mcst != 1f)
+                changes += "mcst,";
+            if (crt != 0)
+                changes += "crt";
+
+            if (dmg != 1f && Math.Round((float)item.damage * dmg) == (double)item.damage)
+            {
+                return false;
+            }
+            if (spd != 1f && Math.Round((float)item.useAnimation * spd) == (double)item.useAnimation)
+            {
+                return false;
+            }
+            if (mcst != 1f && Math.Round((float)item.mana * mcst) == (double)item.mana)
+            {
+                return false;
+            }
+            if (kb != 1f && item.knockBack == 0f)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
---
+++ MyPlayer.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using System.Linq;
+using Experience.Items;
+using System.Drawing;
+
+namespace Experience
+{
+    public class MyPlayer : ModPlayer
+    {
+        // Dictionary: Key = item.type, value = [item experience, item level, currentDamage, exp for next level]
+        public Dictionary<int, int[]> expInfo = new();
+        public bool updateExp = true;
+        //public int[] expByLevel = { 50, 130, 210, 340, 550, 890, 1440, 2330, 3770, 6100, 9780, 15970, 25840, 41810, 67650, 109460, 177110, 286570, 463680, 750250, 1213930, 1964180, 3178110, 5142290, 8320400, 13462690 };
+        public int[] expByLevel = { 100, 300, 600, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 200000, 300000, 1000000 };
+
+        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)   
+        {
+            if(target.life <= 0)
+            {
+                if (expInfo.ContainsKey(item.type))
+                {
+                    expInfo[item.type][0] += target.lifeMax;
+                    expInfo[item.type][3] -= target.lifeMax;
+                }
+                else
+                {
+                    int[] newExpInfo = { target.lifeMax, 0, item.damage, expByLevel[0]};
+                    expInfo.Add(item.type, newExpInfo);
+                }
+                updateExp = true;
+            }
+        }
+
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+        {
+            if (target.life <= 0)
+            {
+                // Get item.type from projectile
+                Item item = this.Player.HeldItem;
+                if (expInfo.ContainsKey(item.type))
+                {
+                    expInfo[item.type][0] += target.lifeMax;
+                    expInfo[item.type][3] -= target.lifeMax;
+                }
+                else
+                {
+                    int[] newExpInfo = { target.lifeMax, 0, item.damage, expByLevel[0] };
+                    expInfo.Add(item.type, newExpInfo);
+                }
+                updateExp = true;                
+            }
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            //base.SaveData(tag);
+            // Saving experience
+            List<int> listkey = expInfo.Keys.ToList();
+            List<int[]> listvalues = new();
+            for(int i = 0; i < listkey.Count; i++) {
+                listvalues.Add(expInfo[listkey[i]]);
+            }
+            tag["expKeys"] = listkey;
+            tag["expValues"] = listvalues;
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            List<int> listkey = tag.Get<List<int>>("expKeys");
+            List<int[]> listvalues = tag.Get<List<int[]>>("expValues");
+            for (int i = 0; i < listkey.Count; i++)
+            {
+                expInfo.Add(listkey[i], listvalues[i]);
+            }
+        }
+
+        public override void PostUpdate() {
+            //base.ModifyStartingInventory(itemsByMod, mediumCoreDeath);
+            if(updateExp) {
+                updateExp = false;
+                Item item = this.Player.HeldItem;
+                int newLevel = 0;
+                int[] newExpInfo = { 0, 0, item.damage, expByLevel[0] };
+                if (!expInfo.ContainsKey(item.type)) {
+                    expInfo.Add(item.type, newExpInfo);
+                }
+                item.damage = expInfo[item.type][2];
+                for (int i = 0; i < expByLevel.Length; i++) {
+                    if(expInfo[item.type][0] >= expByLevel[i]) {
+                        newLevel = i + 1;
+                    } else {
+                        break;
+                    }
+                }
+                // Upgrade damage by amount of level updated newlevel - oldlevel
+                if(newLevel > expInfo[item.type][1]) {
+                    for (int i = expInfo[item.type][1] + 1; i <= newLevel; i++)
+                    {
+                        expInfo[item.type][2] += i;
+                        item.damage = expInfo[item.type][2];
+
+                    }
+                    expInfo[item.type][1] = newLevel;
+                    expInfo[item.type][3] = expByLevel[newLevel] - expInfo[item.type][0];
+
+                    //Update shop value of the item
+                    this.Player.GetItemExpectedPrice(item, out int calcForSelling, out int calcForBuying);
+                    item.shopCustomPrice = calcForSelling + 50*newLevel;
+
+                }
+                //Update Tooltip
+                item.TryGetGlobalItem(out ExpGlobalItem itemInstance);
+                itemInstance.experience = expInfo[item.type];
+
+                
+                // Reset item
+                //newExpInfo[2] = 10;
+                //expInfo[item.type] = newExpInfo;
+            }
+        }
+    }
+}
